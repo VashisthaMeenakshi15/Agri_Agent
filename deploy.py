265f65a59ca13bee3205713556da0ed00e0e8aba
Meenakshi Vashistha <71630955+VashisthaMeenakshi15@users.noreply.github.com>
2026-03-15 16:32:23 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,13 +1,20 @@
 import vertexai
 from vertexai.preview import agent_engines
-from agents.root_agent import root_agent
 
+# import the agent created in agent.py
+from agent import root_agent
+
+# Initialize Vertex AI
 vertexai.init(
-    project="fytzibz-ccai-gcp-sandbox",
+    project="YOUR_PROJECT_ID",   # replace with your project id
     location="us-central1"
 )
 
+
+# Deploy agent to Vertex AI Agent Engine
 agent_engines.create(
-    display_name="agri-agent",
+    display_name="smart-agri-agent",
     agent=root_agent
 )
+
+print("Agent deployment started successfully.")
